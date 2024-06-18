000ed5e10fa961c841437a190e2e1b61d80084c3
Chris <git@chrr.me>
2024-06-18 15:24:33 +0200

    ci: add a release job

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,18 @@
+import me.modmuss50.mpp.ReleaseType
+
 plugins {
     id("fabric-loom") version "1.6-SNAPSHOT"
+    id("me.modmuss50.mod-publish-plugin") version "0.5.1"
 }
 
-val minecraftVersion: String by project
-val yarnMappings: String by project
-val loaderVersion: String by project
-val apiVersion: String by project
+fun Project.prop(namespace: String, key: String) =
+    property("$namespace.$key") as String
 
-val modVersion: String by project
-val mavenGroup: String by project
-val archivesBase: String by project
-
-val fabricVersion: String by project
-val jadeVersionId: String by project
-
-group = mavenGroup
-version = modVersion
+group = prop("mod", "group")
+version = prop("mod", "version")
 
 base {
-    archivesName.set(archivesBase)
+    archivesName.set(prop("mod", "name"))
 }
 
 loom {
@@ -32,11 +26,11 @@ dependencies {
     // To change the versions, see the gradle.properties file
 
     fun fabricApiModule(name: String) =
-        modImplementation(fabricApi.module(name, apiVersion))
+        modImplementation(fabricApi.module(name, prop("fabric", "apiVersion")))
 
-    minecraft("com.mojang:minecraft:$minecraftVersion")
-    mappings("net.fabricmc:yarn:$yarnMappings:v2")
-    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+    minecraft("com.mojang:minecraft:${prop("minecraft", "maxVersion")}")
+    mappings("net.fabricmc:yarn:${prop("fabric", "yarnVersion")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${prop("fabric", "loaderVersion")}")
 
     include(fabricApiModule("fabric-resource-loader-v0")!!)
 }
@@ -45,7 +39,11 @@ tasks {
     processResources {
         inputs.property("version", project.version)
         filesMatching("fabric.mod.json") {
-            expand(project.properties)
+            expand(
+                "modName" to prop("mod", "name"),
+                "modVersion" to prop("mod", "version"),
+                "minecraftDependency" to ">=${prop("minecraft", "minVersion")} <=${prop("minecraft", "maxVersion")}"
+            )
         }
     }
 
@@ -57,4 +55,23 @@ tasks {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+publishMods {
+    displayName.set("${prop("mod", "version")} - Fabric ${prop("minecraft", "maxVersion")}")
+
+    file.set(tasks.remapJar.get().archiveFile)
+    changelog.set(providers.environmentVariable("CHANGELOG"))
+    type.set(if (prop("mod", "version").contains("beta")) ReleaseType.BETA else ReleaseType.STABLE)
+    modLoaders.addAll("fabric", "quilt")
+
+    modrinth {
+        projectId.set(prop("modrinth", "id"))
+        accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
+
+        minecraftVersionRange {
+            start = prop("minecraft", "minVersion")
+            end = prop("minecraft", "maxVersion")
+        }
+    }
 }
