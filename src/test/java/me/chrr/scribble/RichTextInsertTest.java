7f197af740bf03ec9a6b62f96f2dd9c014be1d1a
Andrei Yablonski <9986008+wiskiw@users.noreply.github.com>
2024-08-12 18:18:27 +0200

    Update RichTextInsert tests

--- src/test/java/me/chrr/scribble/RichTextInsertTest.java
+++ src/test/java/me/chrr/scribble/RichTextInsertTest.java
@@ -16,32 +16,37 @@ public class RichTextInsertTest {
     // ---- Plain text insertion tests below ----
 
     @Test
-    public void testPlainTextInsertionAtTheStartOfRichText() {
+    public void testDifferentFormattedTextInsertionAtTheStartOfRichText() {
         String singleSegmentPlainString = "The quick brown fox\njumps over the lazy dog.";
         RichText richText = RichText.fromFormattedString(singleSegmentPlainString);
         assertEquals(1, richText.getSegments().size(), "Unexpected initial state");
+
         String stringToInsert = "boo!";
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, Formatting.WHITE, Set.of(Formatting.OBFUSCATED));
 
+        // Action
         // insert at the very start of rich text
-        richText = richText.insert(0, stringToInsert, Formatting.GOLD, Set.of());
+        richText = richText.insert(0, List.of(segmentToInsert));
 
+        // Assert
         assertEquals(2, richText.getSegments().size());
     }
 
     @Test
-    public void testPlainTextInsertionIntoTheMiddleOfRichTextSegment() {
+    public void testDifferentFormattedTextInsertionIntoTheMiddleOfRichTextSegment() {
         List<RichText.Segment> originSegments = List.of(
                 new RichText.Segment("AA\nAACC\nCC", Formatting.RED, Set.of(Formatting.BOLD)),
                 new RichText.Segment("DDDD", Formatting.BLUE, Set.of(Formatting.UNDERLINE))
         );
         RichText richText = new RichText(List.copyOf(originSegments));
         assertEquals(originSegments.size(), richText.getSegments().size(), "Unexpected initial state");
+
         String stringToInsert = "BBBB";
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, Formatting.WHITE, Set.of(Formatting.OBFUSCATED));
+        int middleOfTheFirstSegmentOffset = originSegments.getFirst().text().length() / 2;
 
         // Action
-        // insert text into the middle of the first segment
-        int middleOfTheFirstSegmentOffset = originSegments.getFirst().text().length() / 2;
-        richText = richText.insert(middleOfTheFirstSegmentOffset, stringToInsert, Formatting.GOLD, Set.of(Formatting.OBFUSCATED));
+        richText = richText.insert(middleOfTheFirstSegmentOffset, List.of(segmentToInsert));
 
         // Assert
         // expecting 4 segments:
@@ -53,38 +58,43 @@ public class RichTextInsertTest {
     }
 
     @Test
-    public void testPlainTextInsertionBetweenRichTextSegments() {
+    public void testDifferentFormattedTextInsertionBetweenRichTextSegments() {
         List<RichText.Segment> originSegments = List.of(
                 new RichText.Segment("AA\nAA", Formatting.RED, Set.of(Formatting.BOLD)),
                 new RichText.Segment("CC\nCC", Formatting.BLUE, Set.of(Formatting.BOLD))
         );
         RichText richText = new RichText(List.copyOf(originSegments));
         assertEquals(originSegments.size(), richText.getSegments().size(), "Unexpected initial state");
+
         String stringToInsert = "BBBB";
-
-        // insert text at the end of the first / at the start of second segment
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, Formatting.WHITE, Set.of(Formatting.OBFUSCATED));
         int endOfTheFirstSegmentOffset = originSegments.getFirst().text().length();
-        richText = richText.insert(endOfTheFirstSegmentOffset, stringToInsert, Formatting.GOLD, Set.of());
 
+        // Action
+        richText = richText.insert(endOfTheFirstSegmentOffset, List.of(segmentToInsert));
+
+        // Asset
         assertEquals(originSegments.size() + 1, richText.getSegments().size());
     }
 
     @Test
-    public void testPlainTextInsertionAtTheEndOfRichText() {
+    public void testDifferentFormattedTextInsertionAtTheEndOfRichText() {
         String singleSegmentPlainString = "The quick brown fox\njumps over the lazy dog.";
         RichText richText = RichText.fromFormattedString(singleSegmentPlainString);
         assertEquals(1, richText.getSegments().size(), "Unexpected initial state");
 
         String stringToInsert = "boo!";
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, Formatting.WHITE, Set.of(Formatting.OBFUSCATED));
 
-        // insert to the very end of rich text
-        richText = richText.insert(richText.getLength(), stringToInsert, Formatting.GOLD, Set.of());
+        // Action
+        richText = richText.insert(richText.getLength(), List.of(segmentToInsert));
 
+        // Asset
         assertEquals(2, richText.getSegments().size());
     }
 
     @Test
-    public void testIfColorStaysCorrectAfterPlainTextInsertionIntoTheMiddleOfRichTextSegment() {
+    public void testIfColorStaysCorrectAfterDifferentFormattedTextInsertionIntoTheMiddleOfRichTextSegment() {
         // Arrange
         List<RichText.Segment> originSegments = List.of(
                 new RichText.Segment("AA\nAA", Formatting.RED, Set.of(Formatting.BOLD)),
@@ -94,12 +104,12 @@ public class RichTextInsertTest {
         assertEquals(originSegments.size(), richText.getSegments().size(), "Unexpected initial state");
 
         String stringToInsert = "BBBB";
-        Formatting stringToInsertColor = Formatting.GOLD;
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, Formatting.WHITE, Set.of(Formatting.OBFUSCATED));
+        int middleOfTheFirstSegmentOffset = originSegments.getFirst().text().length() / 2;
+
 
         // Action
-        // insert text at the end of the first / at the start of second segment
-        int middleOfTheFirstSegmentOffset = originSegments.getFirst().text().length() / 2;
-        richText = richText.insert(middleOfTheFirstSegmentOffset, stringToInsert, stringToInsertColor, Set.of());
+        richText = richText.insert(middleOfTheFirstSegmentOffset, List.of(segmentToInsert));
 
 
         // Assert
@@ -109,13 +119,13 @@ public class RichTextInsertTest {
 
         // check new segment colors
         assertEquals(
-                stringToInsertColor,
+                segmentToInsert.color(),
                 richText.getSegments().get(1).color() // inserted segment
         );
     }
 
     @Test
-    public void testIfModifiersStaysCorrectAfterPlainTextInsertionIntoTheMiddleOfRichTextSegment() {
+    public void testIfModifiersStaysCorrectAfterDifferentFormattedTextInsertionIntoTheMiddleOfRichTextSegment() {
         // Arrange
         List<RichText.Segment> originSegments = List.of(
                 new RichText.Segment("AA\nAA", Formatting.RED, Set.of(Formatting.BOLD)),
@@ -125,18 +135,13 @@ public class RichTextInsertTest {
         assertEquals(originSegments.size(), richText.getSegments().size(), "Unexpected initial state");
 
         String stringToInsert = "BBBB";
-        Set<Formatting> stringToInsertModifiers = Set.of(Formatting.BOLD, Formatting.UNDERLINE);
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, Formatting.WHITE, Set.of(Formatting.OBFUSCATED));
+        int middleOfTheFirstSegmentOffset = originSegments.getFirst().text().length() / 2;
 
 
         // Action
-        // insert text at the end of the first / at the start of second segment
-        int middleOfTheFirstSegmentOffset = originSegments.getFirst().text().length() / 2;
-        richText = richText.insert(
-                middleOfTheFirstSegmentOffset,
-                stringToInsert,
-                Formatting.LIGHT_PURPLE,
-                stringToInsertModifiers
-        );
+        richText = richText.insert(middleOfTheFirstSegmentOffset, List.of(segmentToInsert));
+
 
         // Assert
         // check origin segments colors
@@ -145,36 +150,38 @@ public class RichTextInsertTest {
 
         // check new segment modifiers
         assertEquals(
-                stringToInsertModifiers,
+                segmentToInsert.modifiers(),
                 richText.getSegments().get(1).modifiers() // inserted segment
         );
     }
 
     @Test
-    public void testIfMergingSimilarStyledSegmentsAfterFormattedStringInsertionAtTheStart() {
+    public void testIfMergingSimilarStyledSegmentsAfterStringInsertionAtTheStart() {
         // Arrange
         Formatting color = Formatting.BLUE;
         Set<Formatting> modifiers = Set.of(Formatting.UNDERLINE, Formatting.BOLD);
         Formatting otherColor = Formatting.RED;
         Set<Formatting> otherModifiers = Set.of(Formatting.OBFUSCATED);
-        String formattedString = otherColor
-                + String.join(" ", otherModifiers.stream().map(Formatting::toString).toList())
-                + "formatted-string-text";
+
+        String stringToInsert = "string-text";
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, otherColor, Set.copyOf(otherModifiers));
 
         List<RichText.Segment> originSegments = List.of(
-                new RichText.Segment("AA\nAA", color, modifiers),
-                new RichText.Segment("BB\nBB", color, modifiers),
-                new RichText.Segment("CC\nCC", color, modifiers),
-                new RichText.Segment("DD\nDD", color, modifiers),
-                new RichText.Segment("EE\nEE", color, modifiers)
+                new RichText.Segment("AA\nAA", color, Set.copyOf(modifiers)),
+                new RichText.Segment("BB\nBB", color, Set.copyOf(modifiers)),
+                new RichText.Segment("CC\nCC", color, Set.copyOf(modifiers)),
+                new RichText.Segment("DD\nDD", color, Set.copyOf(modifiers)),
+                new RichText.Segment("EE\nEE", color, Set.copyOf(modifiers))
         );
         RichText richText = new RichText(List.copyOf(originSegments));
         assertEquals(originSegments.size(), richText.getSegments().size(), "Unexpected initial state");
         assertNotEquals(color, otherColor, "Incorrect initial state");
         assertNotEquals(modifiers, otherModifiers, "Incorrect initial state");
 
+
         // Action
-        richText = richText.insert(0, formattedString);
+        richText = richText.insert(0, List.of(segmentToInsert));
+
 
         // Assert
         assertEquals(2, richText.getSegments().size());
@@ -187,66 +194,32 @@ public class RichTextInsertTest {
     }
 
     @Test
-    public void testIfMergingSimilarStyledSegmentsAfterNoFormattedStringInsertion() {
-        // Arrange
-        Formatting color = Formatting.BLUE;
-        Set<Formatting> modifiers = Set.of(Formatting.UNDERLINE, Formatting.BOLD);
-        Formatting otherColor = Formatting.RED;
-        Set<Formatting> otherModifiers = Set.of(Formatting.OBFUSCATED);
-        List<RichText.Segment> originSegments = List.of(
-                new RichText.Segment("AA\nAA", color, modifiers),
-                new RichText.Segment("BB\nBB", color, modifiers),
-                new RichText.Segment("CC\nCC", color, modifiers),
-                new RichText.Segment("--\n--", otherColor, otherModifiers),
-                new RichText.Segment("DD\nDD", color, modifiers),
-                new RichText.Segment("EE\nEE", color, modifiers)
-        );
-        RichText richText = new RichText(List.copyOf(originSegments));
-        assertEquals(originSegments.size(), richText.getSegments().size(), "Unexpected initial state");
-        assertNotEquals(color, otherColor, "Incorrect initial state");
-        assertNotEquals(modifiers, otherModifiers, "Incorrect initial state");
-
-        // Action
-        richText = richText.insert(originSegments.getFirst().text().length() / 2, "non-formatted");
-
-        // Assert
-        assertEquals(3, richText.getSegments().size());
-
-        assertEquals(color, richText.getSegments().get(0).color());
-        assertEquals(modifiers, richText.getSegments().get(0).modifiers());
-
-        assertEquals(otherColor, richText.getSegments().get(1).color());
-        assertEquals(otherModifiers, richText.getSegments().get(1).modifiers());
-
-        assertEquals(color, richText.getSegments().get(2).color());
-        assertEquals(modifiers, richText.getSegments().get(2).modifiers());
-    }
-
-    @Test
     public void testIfMergingSimilarStyledSegmentsAfterFormattedStringInsertionInTheMiddle() {
         // Arrange
         Formatting color = Formatting.BLUE;
         Set<Formatting> modifiers = Set.of(Formatting.UNDERLINE, Formatting.BOLD);
         Formatting otherColor = Formatting.RED;
         Set<Formatting> otherModifiers = Set.of(Formatting.OBFUSCATED);
-        String formattedString = otherColor
-                + String.join(" ", otherModifiers.stream().map(Formatting::toString).toList())
-                + "formatted-string-text";
+
+        String stringToInsert = "string-text";
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, otherColor, Set.copyOf(otherModifiers));
 
         List<RichText.Segment> originSegments = List.of(
-                new RichText.Segment("AA\nAA", color, modifiers),
-                new RichText.Segment("BB\nBB", color, modifiers),
-                new RichText.Segment("CC\nCC", color, modifiers),
-                new RichText.Segment("DD\nDD", color, modifiers),
-                new RichText.Segment("EE\nEE", color, modifiers)
+                new RichText.Segment("AA\nAA", color, Set.copyOf(modifiers)),
+                new RichText.Segment("BB\nBB", color, Set.copyOf(modifiers)),
+                new RichText.Segment("CC\nCC", color, Set.copyOf(modifiers)),
+                new RichText.Segment("DD\nDD", color, Set.copyOf(modifiers)),
+                new RichText.Segment("EE\nEE", color, Set.copyOf(modifiers))
         );
         RichText richText = new RichText(List.copyOf(originSegments));
         assertEquals(originSegments.size(), richText.getSegments().size(), "Unexpected initial state");
         assertNotEquals(color, otherColor, "Incorrect initial state");
         assertNotEquals(modifiers, otherModifiers, "Incorrect initial state");
 
+
         // Action
-        richText = richText.insert(originSegments.get(2).text().length() / 2, formattedString);
+        richText = richText.insert(originSegments.get(2).text().length() / 2, List.of(segmentToInsert));
+
 
         // Assert
         assertEquals(3, richText.getSegments().size());
@@ -268,24 +241,25 @@ public class RichTextInsertTest {
         Set<Formatting> modifiers = Set.of(Formatting.UNDERLINE, Formatting.BOLD);
         Formatting otherColor = Formatting.RED;
         Set<Formatting> otherModifiers = Set.of(Formatting.OBFUSCATED);
-        String formattedString = otherColor
-                + String.join(" ", otherModifiers.stream().map(Formatting::toString).toList())
-                + "formatted-string-text";
+        String stringToInsert = "string-text";
+        RichText.Segment segmentToInsert = new RichText.Segment(stringToInsert, otherColor, Set.copyOf(otherModifiers));
 
         List<RichText.Segment> originSegments = List.of(
-                new RichText.Segment("AA\nAA", color, modifiers),
-                new RichText.Segment("BB\nBB", color, modifiers),
-                new RichText.Segment("CC\nCC", color, modifiers),
-                new RichText.Segment("DD\nDD", color, modifiers),
-                new RichText.Segment("EE\nEE", color, modifiers)
+                new RichText.Segment("AA\nAA", color, Set.copyOf(modifiers)),
+                new RichText.Segment("BB\nBB", color, Set.copyOf(modifiers)),
+                new RichText.Segment("CC\nCC", color, Set.copyOf(modifiers)),
+                new RichText.Segment("DD\nDD", color, Set.copyOf(modifiers)),
+                new RichText.Segment("EE\nEE", color, Set.copyOf(modifiers))
         );
         RichText richText = new RichText(List.copyOf(originSegments));
         assertEquals(originSegments.size(), richText.getSegments().size(), "Unexpected initial state");
         assertNotEquals(color, otherColor, "Incorrect initial state");
         assertNotEquals(modifiers, otherModifiers, "Incorrect initial state");
 
+
         // Action
-        richText = richText.insert(richText.getLength(), formattedString);
+        richText = richText.insert(richText.getLength(), List.of(segmentToInsert));
+
 
         // Assert
         assertEquals(2, richText.getSegments().size());
