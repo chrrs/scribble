7291169fc91dba5d8cab41a2f29d5510398f74a6
Andrei Yablonski <9986008+wiskiw@users.noreply.github.com>
2024-08-14 16:06:07 +0200

    Add history size limit argument to CommandManager

--- src/main/java/me/chrr/scribble/tool/commandmanager/CommandManager.java
+++ src/main/java/me/chrr/scribble/tool/commandmanager/CommandManager.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Objects;
 
 /**
  * Manages a stack of commands and provides undo/redo functionality.
@@ -15,13 +16,21 @@ import java.util.LinkedList;
  */
 public class CommandManager {
 
+    private static final int DEFAULT_HISTORY_SIZE = 30;
     private static final int EMPTY_STACK_INDEX = -1;
 
+    private final int maxHistorySize;
     private final Deque<Command> commandStack;
+
     private int currentIndex = EMPTY_STACK_INDEX;
 
+    public CommandManager(int maxHistorySize) {
+        this.maxHistorySize = maxHistorySize;
+        this.commandStack = new LinkedList<>();
+    }
+
     public CommandManager() {
-        commandStack = new LinkedList<>();
+        this(DEFAULT_HISTORY_SIZE);
     }
 
     public void clear() {
@@ -35,13 +44,32 @@ public class CommandManager {
      * @param command The command to execute.
      */
     public void execute(@NotNull Command command) {
-        while (currentIndex < commandStack.size() - 1) {
-            commandStack.pop();
+        dropAllAboveCurrentIndex();
+
+        if (commandStack.size() >= maxHistorySize) {
+            // size limit was reached
+            // drop a command from the bottom of the stack
+            commandStack.pollLast();
         }
 
         commandStack.push(command);
         command.execute();
         currentIndex++;
+    }
+
+    private void dropAllAboveCurrentIndex() {
+        // pops all commands that above current index
+
+        // For cases like:
+        // execute A - currentIndex at A
+        // execute B - currentIndex at B
+        // execute C - currentIndex at C
+        // undo - moves currentIndex to B
+        // undo - moves currentIndex to A
+        // execute D - will drop all commands that were above currentIndex(above A)
+        while (currentIndex < commandStack.size() - 1) {
+            commandStack.pollFirst();
+        }
     }
 
     /**
@@ -51,7 +79,7 @@ public class CommandManager {
      */
     public boolean tryUndo() {
         if (canUndo()) {
-            commandStack.pop().undo();
+            Objects.requireNonNull(commandStack.pollFirst(), "Check the canUndo() logic.").undo();
             currentIndex--;
             return true;
         }
@@ -64,6 +92,11 @@ public class CommandManager {
      * @return True if an undo operation is possible, false otherwise.
      */
     public boolean canUndo() {
-        return currentIndex >= 0;
+        if (commandStack.isEmpty()) {
+            currentIndex = EMPTY_STACK_INDEX;
+            return false;
+        } else {
+            return currentIndex >= 0;
+        }
     }
 }
--- src/test/java/me/chrr/scribble/tool/commandmanager/CommandManagerTest.java
+++ src/test/java/me/chrr/scribble/tool/commandmanager/CommandManagerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class CommandManagerTest {
 
@@ -11,7 +12,6 @@ public class CommandManagerTest {
 
         @Override
         public void execute() {
-
         }
 
         @Override
@@ -21,7 +21,7 @@ public class CommandManagerTest {
     }
 
     @Test
-    public void testIfCantUndoIfCommandHistoryIsEmpty() {
+    public void testThatCantUndoIfCommandHistoryIsEmpty() {
         CommandManager commandManager = new CommandManager();
 
         // Assert
@@ -37,26 +37,71 @@ public class CommandManagerTest {
     }
 
     @Test
-    public void testIfPutsEveryCommandToCommandStack() {
+    public void testIfPutsCommandToCommandHistoryWhenExecuted() {
         CommandManager commandManager = new CommandManager();
         assertFalse(commandManager.canUndo(), "Unexpected commandManager state");
+        DoNothingCommand doNothingCommand = mock();
 
         // Action
-        commandManager.execute(new DoNothingCommand());
+        commandManager.execute(doNothingCommand);
 
         // Assert
         assertTrue(commandManager.canUndo());
     }
 
     @Test
-    public void testIfCantUndoWhenAllCommandsWereUndo() {
+    public void testIfCantUndoAnymoreWhenAllCommandsWereUndo() {
         CommandManager commandManager = new CommandManager();
+        DoNothingCommand doNothingCommand = mock();
+
 
         // Action
-        commandManager.execute(new DoNothingCommand());
+        commandManager.execute(doNothingCommand);
         assertTrue(commandManager.tryUndo(), "Unexpected commandManager state");
+
 
         // Assert
         assertFalse(commandManager.tryUndo());
+
+        // command's undo should be called only once - on .tryUndo() call
+        verify(doNothingCommand, times(1)).undo();
+    }
+
+    @Test
+    public void testIfDropsFirstExecutedCommandWhenCommandHistorySizeReached() {
+        // Arrange
+        CommandManager commandManager = new CommandManager(3);
+
+        // execute 3 commands to fill up the stack
+        DoNothingCommand commandToBeDroppedFirst = mock();
+        commandManager.execute(commandToBeDroppedFirst);
+
+        DoNothingCommand commandThatWillStay1 = mock();
+        commandManager.execute(commandThatWillStay1);
+
+        DoNothingCommand commandThatWillStay2 = mock();
+        commandManager.execute(commandThatWillStay2);
+
+
+        // Action
+        DoNothingCommand commandThatWillReachTheStackLimit = mock();
+        commandManager.execute(commandThatWillReachTheStackLimit);
+        // call undo for all commands in stack
+        assertTrue(commandManager.tryUndo());
+        assertTrue(commandManager.tryUndo());
+        assertTrue(commandManager.tryUndo());
+
+
+        // Assert
+        // ensure command stack is empty
+        assertFalse(commandManager.canUndo());
+
+        // undo - should not be called for the first added command
+        verify(commandToBeDroppedFirst, never()).undo();
+
+        // ... and should be called for the reset
+        verify(commandThatWillStay1, times(1)).undo();
+        verify(commandThatWillStay2, times(1)).undo();
+        verify(commandThatWillReachTheStackLimit, times(1)).undo();
     }
 }
