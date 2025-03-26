d2abdf08381ded8c261a129091ba978680715fa2
chrrrs <git@chrr.me>
2025-03-26 14:14:38 +0100

    chore: fix compatability with NeoForge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ stonecutter {
         versions("1.20.1", "1.21", "1.21.4", "1.21.5")
         branch("fabric") { versions("1.20.1", "1.21", "1.21.4", "1.21.5") }
         branch("forge") { versions("1.20.1") }
-        branch("neoforge") { versions("1.21", "1.21.4") }
+        branch("neoforge") { versions("1.21", "1.21.4", "1.21.5") }
     }
 }
 
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,7 +2,7 @@ import dev.kikugie.stonecutter.StonecutterProject
 
 plugins {
     id("dev.kikugie.stonecutter")
-    id("dev.architectury.loom") version "1.9-SNAPSHOT" apply false
+    id("dev.architectury.loom") version "1.10-SNAPSHOT" apply false
     id("architectury-plugin") version "3.4-SNAPSHOT" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     id("me.modmuss50.mod-publish-plugin") version "0.5.1" apply false
