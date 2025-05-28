3e942eb4a19b41e821c11a3d1ccde4b7b50651a3
chrrs <git@chrr.me>
2025-05-28 13:41:42 +0200

    chore: re-enable forge 1.20.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ stonecutter {
     create(rootProject) {
         versions("1.20.1", "1.21", "1.21.4", "1.21.5")
         branch("fabric") { versions("1.20.1", "1.21", "1.21.4", "1.21.5") }
-//        branch("forge") { versions("1.20.1") }
+        branch("forge") { versions("1.20.1") }
         branch("neoforge") { versions("1.21", "1.21.4", "1.21.5") }
     }
 }
