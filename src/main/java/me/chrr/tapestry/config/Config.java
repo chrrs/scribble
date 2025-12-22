4de05f4ce3c1a2fa9d243de8bee53054c2475dd7
chrrrs <git@chrr.me>
2025-12-22 23:21:16 +0100

    feat: support showing multiple configs

--- src/main/java/me/chrr/tapestry/config/Config.java
+++ src/main/java/me/chrr/tapestry/config/Config.java
@@ -13,19 +13,10 @@ public interface Config {
 
     ConfigIo.@Nullable UpgradeRewriter getUpgradeRewriter();
 
-    @Nullable String getTranslationPrefix();
+    Component getTitle();
 
     void save();
 
-
-    default Component getText(String key) {
-        String translationPrefix = getTranslationPrefix();
-        if (translationPrefix == null) {
-            return Component.literal(key);
-        } else {
-            return Component.translatable(translationPrefix + "." + key);
-        }
-    }
 
     default void ensureLoaded() {
         // ... empty, if this class is loaded that means the config is loaded.
