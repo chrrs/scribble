016f2c05996f91e2698e5a5a88cd1060db6f38ba
chrrrs <git@chrr.me>
2025-12-28 17:02:08 +0100

    fix: make sure the lectern mixin works on NeoForge

--- src/main/java/me/chrr/scribble/mixin/MenuScreensMixin.java
+++ src/main/java/me/chrr/scribble/mixin/MenuScreensMixin.java
@@ -1,29 +1,35 @@
 package me.chrr.scribble.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import me.chrr.scribble.Scribble;
 import me.chrr.scribble.screen.ScribbleLecternScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.MenuAccess;
 import net.minecraft.world.inventory.LecternMenu;
 import net.minecraft.world.inventory.MenuType;
-import org.jspecify.annotations.Nullable;
+import org.jspecify.annotations.NullMarked;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 
+@NullMarked
 @Mixin(MenuScreens.class)
 public abstract class MenuScreensMixin {
-    @WrapOperation(method = "create", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/MenuScreens;getConstructor(Lnet/minecraft/world/inventory/MenuType;)Lnet/minecraft/client/gui/screens/MenuScreens$ScreenConstructor;"))
-    private static MenuScreens.@Nullable ScreenConstructor<?, ?> overrideLecternScreen(MenuType<LecternMenu> menuType, Operation<MenuScreens.ScreenConstructor<?, ?>> original, @Local(argsOnly = true) Minecraft minecraft) {
-        if (menuType == MenuType.LECTERN) {
-            if (!minecraft.hasShiftDown() || !Scribble.config().openVanillaBookScreenOnShift) {
-                return (MenuScreens.ScreenConstructor<LecternMenu, ScribbleLecternScreen>)
-                        (menu, inventory, title) -> new ScribbleLecternScreen(menu);
-            }
+    @WrapMethod(method = "register")
+    private static <S extends Screen & MenuAccess<LecternMenu>> void overrideLecternScreen(MenuType<LecternMenu> type, MenuScreens.ScreenConstructor<LecternMenu, S> factory, Operation<Void> original) {
+        if (type == MenuType.LECTERN) {
+            original.call(type, (MenuScreens.ScreenConstructor<LecternMenu, S>) (menu, inventory, title) -> {
+                Minecraft minecraft = Minecraft.getInstance();
+                if (!minecraft.hasShiftDown() || !Scribble.config().openVanillaBookScreenOnShift) {
+                    //noinspection unchecked: S is technically not the same here, but it doesn't matter.
+                    return (S) new ScribbleLecternScreen(menu);
+                } else {
+                    return factory.create(menu, inventory, title);
+                }
+            });
+        } else {
+            original.call(type, factory);
         }
-
-        return original.call(menuType);
     }
 }
